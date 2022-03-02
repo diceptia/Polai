b08fcb123cedfcbc0c3fb0dd0a1081f256f1ecbe
laweary <93151356+laweary@users.noreply.github.com>
2022-03-02 14:28:05 +0100

    changing it to

--- src/main.c
+++ src/main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+void marere()
+{
+int pointer = 99;
+int* pointerref = &pointer;
+printf("pointer is = %d \n",pointer);
+*pointerref = 299;
+printf("pointer is = %d \n",pointer);
+
+
+}
 int main(){
-    printf("go lang \n");
+    marere();
 }
